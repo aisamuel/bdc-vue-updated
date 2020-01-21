b51989cfd224c566ea0d5c978a583f52e9acff8b
Amin Balogun <balocodes@gmail.com>
2020-01-21 22:04:57 +0100

    update user profile

--- project/assets/js/profile.js
+++ project/assets/js/profile.js
@@ -1,39 +1,38 @@
 // console.log("Vue app runs")
-
 const profileApp = new Vue({
-  el: "#profileApp",
-  data: {
-    userData: {},
-    updateURL: `${baseURL}reg/update-user/`
-  },
-  methods: {
-    updateProfile: function() {
-      fetch(this.updateURL, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          username: this.userData.username,
-          fname: this.userData.fname,
-          lname: this.userData.lname,
-          address: this.userData.address,
-          city: this.userData.city,
-          country: this.userData.country,
-          aboutMe: this.userData.aboutMe
-        })
-      });
-      console.log(this.userData);
+    el: "#profileApp",
+    data: {
+        userData: {},
+        updateURL: `${baseURL}reg/update-user/`
+    },
+    methods: {
+        updateProfile: async function() {
+            const res = await fetch(`${this.updateURL}${this.userData._id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(this.userData)
+            });
+            if (res) {
+                const result = await res.json();
+                console.log(result)
+                this.setUserProfile(JSON.stringify(this.userData))
+                alert(result.message)
+            }
+            console.log(this.userData);
+        }
+    },
+    mounted: function() {
+        console.log("Vue app mounted");
+        this.setUserProfile = setUserProfile;
+        this.getUserProfile = getUserProfile;
+        this.logout = logout;
+        setTimeout(() => {
+            this.userData = getUserProfile();
+            if (!this.userData) {
+                window.location.href = "/login"
+            }
+        }, 1000)
     }
-  },
-  mounted: function() {
-    console.log("Vue app mounted");
-    this.setUserProfile = setUserProfile;
-    this.getUserProfile = getUserProfile;
-    this.logout = logout;
-    this.userData = getUserProfile();
-    if(!this.userData) {
-        window.location.href = "/login"
-    }
-  }
 });
