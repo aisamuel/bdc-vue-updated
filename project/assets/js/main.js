2a1301fdcbfb91bd96df52d28505f8c3b007780f
sayil1 <sundaysayil4u@gmail.com>
2021-02-13 09:51:07 +0100

    revert to paystack

--- project/assets/js/main.js
+++ project/assets/js/main.js
@@ -17,8 +17,8 @@ const setUserProfile = userProfile => {
 };
 
 const logout = () => {
-    localStorage.removeItem("userProfile")
-    window.location.href = "/landing"
+  localStorage.removeItem("userProfile")
+  window.location.href = "/landing"
 }
 
 const getToken = () => {
@@ -30,9 +30,9 @@ const setToken = token => {
 };
 
 const payWithPaystack = (userProfile, amountInNaira, ref, callback) => {
-    console.log("paying with paystack")
+  console.log("paying with paystack")
   var handler = PaystackPop.setup({
-    key: "pk_test_ad09ec2277fdc777e3eeaa5d51c711ca579a6141",
+    key: "pk_live_560aba442d03bf010d639e0b4f05ee17e9f9575c",
     email: userProfile.email,
     amount: amountInNaira * 100,
     currency: "NGN",
@@ -46,12 +46,12 @@ const payWithPaystack = (userProfile, amountInNaira, ref, callback) => {
         }
       ]
     },
-    callback: function(response) {
+    callback: function (response) {
       alert("success. transaction ref is " + response.reference);
       callback();
 
     },
-    onClose: function() {
+    onClose: function () {
       alert("window closed");
       callback();
     }
@@ -60,37 +60,37 @@ const payWithPaystack = (userProfile, amountInNaira, ref, callback) => {
 };
 
 function payWithFlutterwave(userProfile, amountInNaira, ref, callback) {
-    console.log(amountInNaira);
-    var x = getpaidSetup({
-        PBFPubKey: "FLWPUBK-1b640efbf8ba861d21e53d26854510f8-X",
-        customer_email: userProfile.email,
-        amount: amountInNaira,
-        customer_phone: userProfile.phone,
-        currency: "NGN",
-        txref: ref,
-        meta: [{
-            metaname: "mobileNumber",
-            metavalue: userProfile.phone
-        }],
-        onclose: function() {
-          // alert("window closed");
-          callback();
-        },
-        callback: function(response) {
-            var txref = response.tx.txRef; // collect txRef returned and pass to a           server page to complete status check.
-            console.log("This is the response returned after a charge", response);
-            if (
-                response.tx.chargeResponseCode == "00" ||
-                response.tx.chargeResponseCode == "0"
-            ) {
-                // redirect to a success page
-            } else {
-                // redirect to a failure page.
-            }
+  console.log(amountInNaira);
+  var x = getpaidSetup({
+    PBFPubKey: "FLWPUBK-1b640efbf8ba861d21e53d26854510f8-X",
+    customer_email: userProfile.email,
+    amount: amountInNaira,
+    customer_phone: userProfile.phone,
+    currency: "NGN",
+    txref: ref,
+    meta: [{
+      metaname: "mobileNumber",
+      metavalue: userProfile.phone
+    }],
+    onclose: function () {
+      // alert("window closed");
+      callback();
+    },
+    callback: function (response) {
+      var txref = response.tx.txRef; // collect txRef returned and pass to a           server page to complete status check.
+      console.log("This is the response returned after a charge", response);
+      if (
+        response.tx.chargeResponseCode == "00" ||
+        response.tx.chargeResponseCode == "0"
+      ) {
+        // redirect to a success page
+      } else {
+        // redirect to a failure page.
+      }
 
-            x.close(); // use this to close the modal immediately after payment.
-        }
-    });
+      x.close(); // use this to close the modal immediately after payment.
+    }
+  });
 }
 
 
@@ -102,7 +102,7 @@ const landingNav = new Vue({
   },
   methods: {
     logInOrOut() {
-      if(this.userProfile) {
+      if (this.userProfile) {
         logout()
       } else {
         window.location.href = "login"
