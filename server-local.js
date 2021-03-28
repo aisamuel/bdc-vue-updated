62e10251da370af9b21c6304de7d243e4d37e8be
SAMUEL <sadeniyi@afexnigeria.com>
2021-03-28 16:25:51 +0100

    npm star

--- server-local.js
+++ server-local.js
@@ -2,4 +2,4 @@
 
 const app = require('./server');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(8855, () => console.log('Local app listening on port 3000!'));
